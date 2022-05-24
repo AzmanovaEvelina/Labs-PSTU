2d4fcf425e2fd79580d443b3542582e7a9341e3f
AzmanovaEvelina <93144576+AzmanovaEvelina@users.noreply.github.com>
2022-05-24 19:33:45 +0500

    Update Functions.h

--- Творческие работы/Часть1_Калькулятор_СЛАУ_матричным_методом/Calculator_2/Functions.h
+++ Творческие работы/Часть1_Калькулятор_СЛАУ_матричным_методом/Calculator_2/Functions.h
@@ -4,5 +4,5 @@
 #include <cmath>
 #include<fstream>
 using namespace System;
-std::string& Convert_String_to_string(String^ s, std::string& os);
-String^ Convert_string_to_String(std::string& os, String^ s);
+std::string& Convert_String_to_string(String^ s, std::string& os); // Конвертируем System:: srting^ в std::string
+String^ Convert_string_to_String(std::string& os, String^ s); // Конвертируем std::string в System:: srting^ 
